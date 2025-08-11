e8e590ea27bc208a9eab93ddd6c77872dd16bf09
kwanelexavi <kwanelexavi@gmail.com>
2025-08-12 00:00:28 +0200

    my list task

---
+++ list.py
@@ -0,0 +1,31 @@
+# my list 
+
+my_list = []
+
+# add elements to my list
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# insert 15 in 2nd postion
+
+my_list.insert(1,15)
+
+# extend my list
+
+my_list.extend([50, 60, 70])
+
+# remove the last element 
+
+my_list.pop()
+
+# sort in ascending order
+
+my_list.sort()
+
+# find and print index value 30
+
+i = my_list.index(30)
+print("i =",i)
